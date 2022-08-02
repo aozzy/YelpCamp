2fd68d90b7aa0f699d1dd8135ac3af30b81e3b99
aosman <pianogreen0@gmail.com>
2022-08-02 23:59:28 +0100

    ready

--- app.js
+++ app.js
@@ -171,7 +171,7 @@ app.use((err, req, res, next) => {
   if (!err.message) err.message = "oh no, something went wrong";
   res.status(statusCode).render("error", { err });
 });
-
-app.listen(3000, () => {
-  console.log("Listeneting on port 3000");
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`Listeneting on port ${port}`);
 });
